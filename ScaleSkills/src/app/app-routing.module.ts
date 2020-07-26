3f3977a57adf93ea5ee96845624bef55389d7775
Santhosh <santhoshkumarbss@gmail.com>
2020-07-26 10:43:05 -0400

    added new setup

--- ScaleSkills/src/app/app-routing.module.ts
+++ ScaleSkills/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ExamComponent } from './exam/exam.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'exam', component:ExamComponent},
+  {path: 'dashboard', component: DashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
